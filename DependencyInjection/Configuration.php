f941b3a59ef9a406ae36a3a1ca8813399c5b851f
ialert <info@ialert.name>
2018-02-05 20:13:34 +0200

    Bundle DI extension was added

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ialert\TelegramBotBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('ialert_telegrambot');
+
+        $rootNode
+            ->children()
+            ->arrayNode('api')->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('username')->isRequired()->end()
+            ->scalarNode('token')->isRequired()->end()
+            ->end()
+            ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ DependencyInjection/IalertTelegramBotExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ialert\TelegramBotBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class IalertTelegramBotExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $loader->load('services.yaml');
+
+        $container->setParameter('ialert_telegram_bot.api.token', $config['api']['token']);
+        $container->setParameter('ialert_telegram_bot.api.username', $config['api']['username']);
+    }
+}
