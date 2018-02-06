f39448ee1423932fd4328f92d48f795792325d56
ialert <info@ialert.name>
2018-02-06 15:07:09 +0200

    removed unused webhook event

--- Event/Telegram/TelegramEvent.php
+++ Event/Telegram/TelegramEvent.php
@@ -7,5 +7,4 @@ namespace Ialert\TelegramBotBundle\Event\Telegram;
 final class TelegramEvent
 {
     public const UPDATE = 'ialert_telegram_bot.event.update';
-    public const WEBHOOK = 'ialert_telegram_bot.event.webhook';
 }
