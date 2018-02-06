f0315cf13898f0d49123dd319831ee12cbcb55e6
ialert <info@ialert.name>
2018-02-06 15:04:59 +0200

    handle webhook update method was added

--- Telegram/TelegramApi.php
+++ Telegram/TelegramApi.php
@@ -4,9 +4,59 @@ declare(strict_types=1);
 
 namespace Ialert\TelegramBotBundle\Telegram;
 
-class TelegramApi
+use Ialert\TelegramBotBundle\Event\Telegram\TelegramEvent;
+use Ialert\TelegramBotBundle\Event\Telegram\UpdateEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use TelegramBot\Api\BotApi;
+use TelegramBot\Api\Types\Update;
+
+class TelegramApi implements TelegramApiInterface
 {
-    public function __construct()
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher)
     {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @throws BadRequestHttpException
+     *
+     * @return Response
+     */
+    public function handleUpdate(Request $request): Response
+    {
+        try {
+            $data = BotApi::jsonValidate($request->getContent(), true);
+        } catch (\Exception $e) {
+            throw new BadRequestHttpException('Empty data');
+        }
+
+        /** @var Update $update */
+        $update = Update::fromResponse($data);
+        $event = $this->processUpdate($update);
+
+        return $event->getResponse() ? $event->getResponse() : new Response();
+    }
+
+    /**
+     * @param Update $update
+     *
+     * @return UpdateEvent
+     */
+    public function processUpdate(Update $update): UpdateEvent
+    {
+        $event = new UpdateEvent($update);
+        $this->eventDispatcher->dispatch(TelegramEvent::UPDATE, $event);
+
+        return $event;
     }
 }
